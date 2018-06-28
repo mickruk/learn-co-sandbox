688fdef7858926a4a501e9efca37fd565cf647c9
Michelle Kruk <kruk.mich@gmail.com>
2018-06-28 21:19:12 +0000

    automatically backed up by learn

---
+++ Fortune_Teller/model.rb
@@ -0,0 +1,7 @@
+def get_fortune()
+  fortunes_array = ["you will win 20 goats soon", "you will marry Shawn Mendez", "you will find $20 but then spend it on a cheap sweater", "you will be awarded a Nobel Prize for computer science research"]
+  return fortunes_array.sample
+end 
+
+
+  
